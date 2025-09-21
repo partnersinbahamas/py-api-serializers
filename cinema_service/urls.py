6c0e09a0fbd387bf0cae240603eefdeb2b669289
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-09-21 18:39:13 +0200

    feat: add django_debug_toolbar

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path(settings.BASE_API_URL, include("cinema.urls"))
+] + debug_toolbar_urls()

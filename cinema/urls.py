804f9a4031435c055d764795a4de44c347e3cd24
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-09-21 19:20:14 +0200

    feat(urls): create views routes

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,45 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+from cinema.views import (
+    MovieViewSet,
+    MovieSessionViewSet,
+    GenreViewSet,
+    CinemaHallViewSet,
+    ActorViewSet
+)
+
+router = routers.DefaultRouter()
+
+router.register(
+    "movies",
+    MovieViewSet,
+    basename="movies"
+)
+
+router.register(
+    "movie_sessions",
+    MovieSessionViewSet,
+    basename="movie_sessions"
+)
+
+router.register(
+    "genres",
+    GenreViewSet,
+    basename="genres"
+)
+
+router.register(
+    "cinema_halls",
+    CinemaHallViewSet,
+    basename="cinema_halls"
+)
+
+router.register(
+    "actors",
+    ActorViewSet,
+    basename="actors"
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
